e27f08d4cef830d485d9049c87e85432b719ac15
Maxim <gatilin@teamidea.ru>
2017-03-31 17:18:02 +0300

    #9 Back button has been added

--- js/main.js
+++ js/main.js
@@ -2,8 +2,13 @@
     const generator = new StyleLintOncogene(options);
     const dismissBtn = document.querySelector('.js-dismiss-btn');
     const resetBtn = document.querySelector('.js-reset-btn');
+    const backBtn = document.querySelector('.js-back-btn');
 
     dismissBtn.addEventListener('click', () => {
         generator.nextStep();
+    });
+
+    backBtn.addEventListener('click', () => {
+        generator.prevStep();
     });
 })();
